1c79a8caa5d3f2774ac77f0b8f5668e3f5caf3c7
RohitDhankar <dhankar.rohit@gmail.com>
2020-10-02 17:10:41 +0530

    flaskMongo.py >> read_dict

--- flaskMongo.py
+++ flaskMongo.py
@@ -6,22 +6,31 @@
 # conn is then imported.
 # os.getcwd() returns the absolute path of the working directory where Python is currently running as a string str.
 
-
+"""
+Source - https://docs.python.org/3/library/csv.html#csv.DictReader
+    #The fieldnames parameter is a sequence. 
+    If fieldnames is omitted, the values in the first
+     row of file f will be used as the fieldnames.
+      Regardless of how the fieldnames are determined, 
+      the dictionary preserves their original ordering.
+"""
+    
 import json, csv, sys, os
-sys.path.append(os.getcwd()+'/classes')
-import conn
+#sys.path.append(os.getcwd()+'/classes')
+#import conn
 
-def read_dict(f, h):
-    input_file = csv.DictReader(open(f), fieldnames=h)
+def read_dict(f):#, h):
+    input_file = csv.DictReader(open(f))#, fieldnames=h)
+    #print(type(input_file)) #<class 'csv.DictReader'>
     return (input_file)
 
 def conv_reg_dict(d):
     return [dict(x) for x in d]
 
-def dump_json(f, d):
+def dump_json(file_json, dict_input):
     """ writes JSON data to disk"""
-    with open(f, 'w') as f:
-        json.dump(d, f)
+    with open(file_json, 'w') as file_json:
+        json.dump(dict_input, file_json)
 
 def read_json(f):
     """ reads JSON data from disk"""
@@ -29,48 +38,58 @@ def read_json(f):
         return json.load(f)
 
 if __name__ == '__main__':
-    f = 'data/names.csv'
-    headers = ['first', 'last']
-    r_dict = read_dict(f, headers)
+    csv_file_in = '/home/dhankar/temp/flask/1/learn-flask/mysql/csvData/mtcars.csv' # FoobarABS path -- change t relative
+    #headers = ['first', 'last'] # this is Read from File FIRST ROW if left as None
+    # Above line - headers == fieldnames of the == class 'csv.DictReader' -- FOOBAR See comment above 
+    r_dict = read_dict(csv_file_in)#, headers)
+    #print("--type(r_dict)---",type(r_dict)) #<class 'csv.DictReader'>
+    #
     dict_ls = conv_reg_dict(r_dict)
-    json_file = 'data/names.json'
-    dump_json(json_file, dict_ls)
+    json_file = '/home/dhankar/temp/flask/1/learn-flask/mongo_testData/test.json'
+    dump_json(json_file, dict_ls) #def dump_json(file_json, dict_input):
     data = read_json(json_file)
-    obj = conn.conn('test')
-    db = obj.getDB()
-    names = db.names
-    names.drop()
-    for i, row in enumerate(data):
-        row['_id'] = i
-        names.insert_one(row)
-    n = 3
-    print('1st', n, 'names:')
-    people = names.find()
-    for i, row in enumerate(people):
-        if i < n:
-            print (row)
-    people.rewind()
-    print('\n1st', n, 'names with rewind:')    
-    for i, row in enumerate(people):
-        #
-        if i < n:
-            print (row)
-    print ('\nquery 1st', n, 'names')
-    first_n = names.find().limit(n)
-    for row in first_n:
-        print (row)
-    print ('\nquery last', n, 'names')
-    length = names.find().count()
-    last_n = names.find().skip(length - n)
-    for row in last_n:
-        print (row)
-    fnames = ['Ella', 'Lou']
-    lnames = ['Vader', 'Pole']    
-    print ('\nquery Ella:')
-    query_1st_in_list = names.find( {'first':{'$in':[fnames[0]]}})
-    for row in query_1st_in_list:
-        print (row)
-    print ('\nquery Ella or Lou:')
-    query_1st = names.find( {'first':{'$in':fnames}} )
-    for row in query_1st:
-        print (row)
+    print(type(data)) # <class 'list'>
+    print(data)
+
+
+    # obj = conn.conn('test')
+    # db = obj.getDB()
+    # names = db.names
+    # names.drop()
+    # for i, row in enumerate(data):
+    #     row['_id'] = i
+    #     names.insert_one(row)
+    # n = 3
+    # print('1st', n, 'names:')
+    # people = names.find()
+    # for i, row in enumerate(people):
+    #     if i < n:
+    #         print (row)
+    # people.rewind()
+    # print('\n1st', n, 'names with rewind:')    
+    # for i, row in enumerate(people):
+    #     #
+    #     if i < n:
+    #         print (row)
+    # print ('\nquery 1st', n, 'names')
+    # first_n = names.find().limit(n)
+    # for row in first_n:
+    #     print (row)
+    # print ('\nquery last', n, 'names')
+    # length = names.find().count()
+    # last_n = names.find().skip(length - n)
+    # for row in last_n:
+    #     print (row)
+    # fnames = ['Ella', 'Lou']
+    # lnames = ['Vader', 'Pole']    
+    # print ('\nquery Ella:')
+    # query_1st_in_list = names.find( {'first':{'$in':[fnames[0]]}})
+    # for row in query_1st_in_list:
+    #     print (row)
+    # print ('\nquery Ella or Lou:')
+    # query_1st = names.find( {'first':{'$in':fnames}} )
+    # for row in query_1st:
+    #     print (row)
+
+
+
