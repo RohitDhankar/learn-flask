121874ca1b00b354aaee6f3c7bbf03598c4a4223
RohitDhankar <dhankar.rohit@gmail.com>
2020-10-04 21:14:50 +0530

    Flask in a Docker container http://localhost:56733/

--- mongo_all/docker_all/var_d/www_d/TestApp/app/views.py
+++ mongo_all/docker_all/var_d/www_d/TestApp/app/views.py
@@ -1,5 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 def home():
    return "hello world!
+
+@app.route('/template')
+def template():
+   return render_template('home.html')
+
+
+   
